91866433ca2994c07934a39fc0c1cce3fe068092
PozigunDiana <92393004+PozigunDiana@users.noreply.github.com>
2022-01-12 14:59:02 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.cap-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    animateIn: 'flipInX'
+});
+$('.base-carousel-3').owlCarousel({
+    loop: false,
+    margin: 20,
+    nav: true,
+    items: 3
+});
+$('.base-carousel-6').owlCarousel({
+    loop: false,
+    margin: 20,
+    nav: true,
+    items: 6
+});
